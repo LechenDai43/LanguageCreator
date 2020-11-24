42d02af69bfbe43df8cf263e26981aee6881a6fc
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-24 00:44:53 -0500

    initial screen locale

---
+++ Assets/Scripts/UI/NewButtonText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewButtonText : MonoBehaviour
+{
+    public Text text;
+    private Manager manager;
+    private int locale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = Object.FindObjectOfType<Manager>();
+        locale = manager.localeManager.getCurrentLocale();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (locale != manager.localeManager.localePointer)
+        {
+            locale = manager.localeManager.getCurrentLocale();
+            updateText();
+        }
+
+    }
+
+    void updateText()
+    {
+        string textContent = manager.localeManager.getInitialScreenLocale().NewButton;
+        text.text = textContent;
+    }
+}
