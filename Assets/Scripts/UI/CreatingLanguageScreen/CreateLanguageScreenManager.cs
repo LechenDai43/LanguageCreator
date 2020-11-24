1dde7c86ef9ac6d6377016ce88de96d6906efda2
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-24 12:37:31 -0500

    back/next page button functions

---
+++ Assets/Scripts/UI/CreatingLanguageScreen/CreateLanguageScreenManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenManager : MonoBehaviour
+{
+    // The pages in this scene
+    public GameObject[] panels;
+    private int panelPointer;
+
+    // The text that needs to change locale but not in pages
+    public Text backButtonText;
+    public Text nextButtonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Go to the previous page
+    public void backPage()
+    {
+
+    }
+
+    // Go to the next page
+    public void nextPage()
+    {
+
+    }
+}
