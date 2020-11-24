794e792025fce2a02fa42f9f1147986fd2d6240e
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-24 13:47:33 -0500

    more on phone prototype

---
+++ Assets/Scripts/Objects/Phone/ConsonantPhone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class ConsonantPhone : ProtoPhone
+{
+    public string POA;
+    public string MOA;
+    public string Aspiration;
+    public string Voiceness;
+}
---
+++ Assets/Scripts/Objects/Phone/ProtoPhone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class ProtoPhone
+{
+    public string IPA;
+    public string FCB;
+}
