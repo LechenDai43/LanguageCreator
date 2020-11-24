c45aa26dfd7a27c0554f65c762f033e635851ebb
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-24 01:06:17 -0500

    locale dropdown

--- Assets/Scripts/UI/LocalDropDown.cs
+++ Assets/Scripts/UI/LocalDropDown.cs
@@ -5,11 +5,34 @@ using UnityEngine.UI;
 
 public class LocalDropDown : MonoBehaviour
 {
+    public Dropdown dropDown;
 
-    public void ChangeLocale(Dropdown change)
+    public void ChangeLocale()
     {
         Manager manager = Object.FindObjectOfType<Manager>();
-        manager.changeLocale(change.value);
+        manager.changeLocale(dropDown.value);
+    }
+
+    void Start()
+    {
+        // Clear the default options
+        dropDown.ClearOptions();
+
+        // Get the string list of the new options
+        Manager manager = Object.FindObjectOfType<Manager>();
+        string[] localeStringArray = manager.localeManager.getAllLocales();
+
+        // Convert the string array to option list
+        List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
+        for (int i = 0; i < localeStringArray.Length; i++)
+        {
+            Dropdown.OptionData optionData = new Dropdown.OptionData();
+            optionData.text = localeStringArray[i];
+            options.Add(optionData);
+        }
+
+        // Set the option list to the drop down list
+        dropDown.options = options;
     }
 
     
