6c6978e1f271e67f039118217dc92da74c064ce7
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-24 21:36:24 -0500

    start layout page 2 of create language screen

---
+++ Assets/Scripts/UI/CreatingLanguageScreen/SubCanvasScaler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubCanvasScaler : MonoBehaviour
+{
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = this.transform.parent.GetComponent<RectTransform>().rect.height;
+        float width = this.transform.parent.GetComponent<RectTransform>().rect.width;
+        float x = this.transform.parent.GetComponent<RectTransform>().rect.x;
+        float y = this.transform.parent.GetComponent<RectTransform>().rect.y;
+        // this.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, y / 2);
+        // this.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, x);
+
+        if (index == 0)
+        {
+            this.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, height / 2, height / 2);
+        }
+        else if (index == 1)
+        {
+            this.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, height / 2, height / 2);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
