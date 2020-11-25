67722fbd76f91def14d24154480cca18df928e2a
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-25 01:41:06 -0500

    Test Drag and Over Size Item Prefab

--- Assets/Scripts/UI/CreatingLanguageScreen/PhoneElementItem.cs
+++ Assets/Scripts/UI/CreatingLanguageScreen/PhoneElementItem.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PhoneElementItem : MonoBehaviour
+public class PhoneElementItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     public ProtoPhone phone;
+    private RectTransform rectTransform;
+    private Vector2 delta;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        delta = new Vector2(0.0f, 0.0f);
+
     }
 
     // Update is called once per frame
@@ -19,5 +24,25 @@ public class PhoneElementItem : MonoBehaviour
         {
             
         }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+        delta += eventData.delta;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        delta = new Vector2(0.0f, 0.0f);
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition -= delta;
     }
 }
