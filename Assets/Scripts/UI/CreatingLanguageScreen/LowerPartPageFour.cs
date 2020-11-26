ad1ed65d1c4ec5da34c5a785665a7180bb066597
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-26 04:55:49 -0500

    lower layout of page four

---
+++ Assets/Scripts/UI/CreatingLanguageScreen/LowerPartPageFour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerPartPageFour : MonoBehaviour
+{
+    public int row, column;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = this.transform.parent.GetComponent<RectTransform>().rect.height;
+        float width = this.transform.parent.GetComponent<RectTransform>().rect.width;
+        this.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, width * column / 3, width / 3);
+        this.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, height * row / 2, height / 2);
+    }
+}
