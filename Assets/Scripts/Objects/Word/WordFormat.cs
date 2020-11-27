f18e122c382748686ac58cee3baa4b5e1ec66175
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-27 03:18:26 -0500

    word format object

---
+++ Assets/Scripts/Objects/Word/WordFormat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordFormat
+{
+    public int numOfSyllable;
+    public AccentRule[] accentRules;
+    public Phoneme specialLeading, specialEnding;
+    public Phoneme[] specialVowel, specialConsonant;
+    public bool arabicStyle, consonantWithSemivowel;
+    public Phoneme[] vowelHolders;
+
+
+    public class AccentRule
+    {
+        public int position;
+        public bool backword;
+        public AccentPhone[] accents;
+    }
+}
