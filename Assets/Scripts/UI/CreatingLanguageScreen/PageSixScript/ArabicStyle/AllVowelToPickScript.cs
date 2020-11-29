849a64123229c8f0d7e9496f2f56179d90fd3f7b
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-11-28 21:47:40 -0500

    declare event handler function

--- Assets/Scripts/UI/CreatingLanguageScreen/PageSixScript/ArabicStyle/AllVowelToPickScript.cs
+++ Assets/Scripts/UI/CreatingLanguageScreen/PageSixScript/ArabicStyle/AllVowelToPickScript.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AllVowelToPickScript : MonoBehaviour
 {
+    public List<Toggle> allToggles = new List<Toggle>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,15 @@ public class AllVowelToPickScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void onCheckButtonPressed()
+    {
+
+    }
+
+    public void onCancelButtonPressed()
+    {
+        Destroy(this.transform.gameObject);
     }
 }
--- Assets/Scripts/UI/CreatingLanguageScreen/PageSixScript/ArabicStyle/PickVowelForArabicStylePageSix.cs
+++ Assets/Scripts/UI/CreatingLanguageScreen/PageSixScript/ArabicStyle/PickVowelForArabicStylePageSix.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickVowelForArabicStylePageSix : MonoBehaviour
 {
+    public Toggle toggle;
+    public Text IPA, letter;
+    public AllVowelToPickScript parent;
+    public int index;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,10 @@ public class PickVowelForArabicStylePageSix : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void onToggleChange()
+    {
+
     }
 }
