4999bdd353ec426248e4bb8196bedf2851a7fc03
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-12-06 04:14:15 -0500

    read vocabulary

--- Assets/Scripts/UI/LanguageScreen/LanguageScreenManager.cs
+++ Assets/Scripts/UI/LanguageScreen/LanguageScreenManager.cs
@@ -23,11 +23,53 @@ public class LanguageScreenManager : MonoBehaviour
 
         string directoryPath = Application.dataPath + currentLanguage.Directory;
         DirectoryInfo dir = new DirectoryInfo(directoryPath);
-        FileInfo[] fileInfos = dir.GetFiles("*.*");
-        Debug.Log(fileInfos.Length);
+        FileInfo[] fileInfos = dir.GetFiles("*.vocabulary");
         foreach (FileInfo ff in fileInfos)
         {
-            Debug.Log(ff.Name);
+            Debug.Log(ff.FullName);
+
+            string front = ff.Name.Substring(0, ff.Name.LastIndexOf("-"));
+            string behind = ff.Name.Substring(ff.Name.LastIndexOf("-") + 1);
+
+            try
+            {
+                int index = int.Parse(behind);
+                Vocabulary[] holder = null;
+                if (front.Equals("others"))
+                {
+                    holder = others;
+                }
+                else if (front.Equals("verbs"))
+                {
+                    holder = verbs;
+                }
+                else if (front.Equals("nouns"))
+                {
+                    holder = nouns;
+                }
+                else if (front.Equals("adjectives"))
+                {
+                    holder = adjectives;
+                }
+
+                if (holder != null)
+                {
+                    string consonantPath = Application.dataPath + ff.FullName;
+                    StreamReader sr = new StreamReader(consonantPath);
+                    string content = sr.ReadToEnd();
+                    sr.Close();
+                    Vocabulary vocabulary = JsonUtility.FromJson<Vocabulary>(content);
+
+                    if (currentLanguage.Name.Equals(vocabulary.Name))
+                    {
+                        holder[index] = vocabulary;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
         }
     }
 
