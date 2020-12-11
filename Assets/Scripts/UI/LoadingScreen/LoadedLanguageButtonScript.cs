97b5c2672956f9239ee1cde9807b14488ebfb1ea
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-12-11 01:00:52 -0500

    loading screen prefabed button

---
+++ Assets/Scripts/UI/LoadingScreen/LoadedLanguageButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadedLanguageButtonScript : MonoBehaviour
+{
+    public Text displayedText;
+    public LanguageFamily language;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickThisButton()
+    {
+        if (language != null) {
+            Manager manager = UnityEngine.Object.FindObjectOfType<Manager>();
+            manager.currentLanguage = language;
+            SceneManager.LoadScene("LanguageScreen");
+        }
+    }
+}
