6a64df1a610425ad5e02eb53982f7d766ca2cec9
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-12-10 19:57:38 -0500

    check out specified vowel

--- Assets/Scripts/Objects/ObjectForSaveLanguage/Sandhi.cs
+++ Assets/Scripts/Objects/ObjectForSaveLanguage/Sandhi.cs
@@ -40,9 +40,11 @@ public class Sandhi
         // TODO...
         // find the target pattern
         // change the target pattern to the desired pattern
+        Manager manager = UnityEngine.Object.FindObjectOfType<Manager>();
+        PhoneManager phoneManager = manager.phoneManager;
 
         int i = 0;
-        while (i < list.Count - Target.Length)
+        while (i < list.Count - Target.Length + 1)
         {
             if (list[i].Level == null || list[i].Level.Equals(""))
             {
@@ -86,10 +88,21 @@ public class Sandhi
                 else if (Target[j].SingleHolder && Target[j].Type.Contains("w"))
                 {
                     List<Phone> tem = new List<Phone>();
+                    if (i + length >= list.Count)
+                    {
+                        fullMatch = false;
+                        break;
+                    }
                     if (list[i + length].Openness != null && !list[i + length].Openness.Equals(""))
                     {
                         tem.Add(list[i + length]);
                         length++;
+
+                        if (i + length >= list.Count)
+                        {
+                            fullMatch = false;
+                            break;
+                        }
                         if (list[i + length].Level != null && !list[i + length].Level.Equals(""))
                         {
                             tem.Add(list[i + length]);
@@ -107,6 +120,12 @@ public class Sandhi
                 else if (Target[j].SingleHolder && Target[j].Type.Contains("c"))
                 {
                     List<Phone> tem = new List<Phone>();
+
+                    if (i + length >= list.Count)
+                    {
+                        fullMatch = false;
+                        break;
+                    }
                     if (list[i + length].POA != null && !list[i + length].POA.Equals(""))
                     {
                         tem.Add(list[i + length]);
@@ -124,11 +143,24 @@ public class Sandhi
                 {
                     List<Phone> tem = new List<Phone>();
                     int count = 0;
-                    while (list[i + length].Openness != null && !list[i + length].Openness.Equals(""))
+
+                    if (i + length >= list.Count)
+                    {
+                        fullMatch = false;
+                        break;
+                    }
+                    while (i + length < list.Count && list[i + length].Openness != null && !list[i + length].Openness.Equals(""))
                     {
                         count++;
+
                         tem.Add(list[i + length]);
                         length++;
+
+                        if (i + length >= list.Count)
+                        {
+                            fullMatch = false;
+                            break;
+                        }
                         if (list[i + length].Level != null && !list[i + length].Level.Equals(""))
                         {
                             tem.Add(list[i + length]);
@@ -151,7 +183,13 @@ public class Sandhi
                 {
                     List<Phone> tem = new List<Phone>();
                     int count = 0;
-                    while (list[i + length].POA != null && !list[i + length].POA.Equals(""))
+
+                    if (i + length >= list.Count)
+                    {
+                        fullMatch = false;
+                        break;
+                    }
+                    while (i + length < list.Count && list[i + length].POA != null && !list[i + length].POA.Equals(""))
                     {
                         count++;
                         tem.Add(list[i + length]);
@@ -168,7 +206,84 @@ public class Sandhi
                     }
 
                 }
+                // If this is a specified vowel
+                else if (Target[j].Type.Contains("w"))
+                {
+                    List<Phone> tem = new List<Phone>();
 
+
+                    if (i + length >= list.Count)
+                    {
+                        fullMatch = false;
+                        break;
+                    }
+                    int row = Target[j].Roundness;
+                    int column = Target[j].Openness;
+
+                    bool isOpen = false, isRound = false;
+
+                    if (row > phoneManager.vowelPool.Length)
+                    {
+                        isRound = true;
+                    }
+                    else if (row >= 0)
+                    {
+                        int poolIndex = 0;
+                        while (poolIndex < phoneManager.vowelPool[row].Length)
+                        {
+                            if (phoneManager.vowelPool[row][poolIndex] != null)
+                            {
+                                if (list[i + length].Roundness.Equals(phoneManager.vowelPool[row][poolIndex].Roundness))
+                                {
+                                    isRound = true;
+                                }
+                                break;
+                            }
+                            poolIndex++;
+                        }
+                    }
+
+                    if (column > phoneManager.vowelPool[0].Length)
+                    {
+                        isOpen = true;
+                    }
+                    else if (column >= 0)
+                    {
+                        int poolIndex = 0;
+                        while (poolIndex < phoneManager.vowelPool.Length)
+                        {
+                            if (phoneManager.vowelPool[poolIndex][column] != null)
+                            {
+                                if (list[i + length].Openness.Equals(phoneManager.vowelPool[poolIndex][column].Openness))
+                                {
+                                    if (list[i + length].FCB.Equals(phoneManager.vowelPool[poolIndex][column].FCB))
+                                    {
+                                        isOpen = true;
+                                    }
+                                }
+                                break;
+                            }
+                            poolIndex++;
+                        }
+                    }
+
+                    if (isRound && isOpen)
+                    {
+                        tem.Add(list[i + length]);
+                        length++;
+                        if (list[i + length].Level != null && !list[i + length].Level.Equals(""))
+                        {
+                            tem.Add(list[i + length]);
+                            length++;
+                        }
+                        founded.Add(tem);
+                    }
+                    else
+                    {
+                        fullMatch = false;
+                        break;
+                    }
+                }
             }
         }
 
