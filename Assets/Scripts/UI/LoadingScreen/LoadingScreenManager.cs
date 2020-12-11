ec6d8b8858045931a0081280ec6f475cb12cc2f8
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-12-11 01:18:09 -0500

    loading game

---
+++ Assets/Scripts/UI/LoadingScreen/LoadingScreenManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    public GameObject buttonContent;
+    public GameObject prefabedButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string directoryPath = Application.dataPath + "Files/Customization/Languages";
+        DirectoryInfo dir = new DirectoryInfo(directoryPath);
+        FileInfo[] fileInfos = dir.GetFiles("*.languageFamily");
+        foreach (FileInfo ff in fileInfos)
+        {
+            try
+            {
+                string filePath = Application.dataPath + ff.FullName;
+                string name = ff.Name.Replace(".languageFamily", "");
+
+                GameObject instanceButton = (GameObject)Instantiate(prefabedButton, buttonContent.transform);
+                LoadedLanguageButtonScript buttonScript = instanceButton.GetComponent<LoadedLanguageButtonScript>();
+                buttonScript.displayedText.text = name;
+                buttonScript.languagePath = filePath;
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void backToHome()
+    {
+
+        SceneManager.LoadScene("InitialScreen");
+    }
+}
